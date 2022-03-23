1badc37cab5a40d47f2d0bc769206ee37312904c
Jonas Stettner <mail@jstet.net>
2022-03-24 00:25:26 +0100

    Update server.R

--- App/server.R
+++ App/server.R
@@ -15,14 +15,7 @@ server <- function(input, output, session){
     observeEvent(input$submit_link,
         output$error <- renderText(as.character(calculate_difference(HeaderValue, input$spotify_link, get_boogie_metrics(HeaderValue = HeaderValue))))
     )
-    
-    ##To do: hide video at first, instead display something else
-    #show different videos when answer changes
-    #change playback speed of video -> calculate speed change through bpm
-    #cut video so it fits to bpm
-    #make spotify play the song
-    #when user pauses, video should pause too
-    #maybe optimize for mobile devices
+   
     
 }
 
